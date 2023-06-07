63c63b46d1b2a78906294b633e7d8b335ce14353
josephsumabat <joseph@mercury.com>
2023-06-07 14:33:18 -0400

    make test field consistent
    Co-authored-by: Barry Moore <3086255+chiroptical@users.noreply.github.com>

--- test/SumOfProductWithTaggedObjectAndSingleNullarySpec.hs
+++ test/SumOfProductWithTaggedObjectAndSingleNullarySpec.hs
@@ -32,7 +32,7 @@ mobileGenWith
             { encodingStyle = TaggedObjectStyle
             , sumAnnotations = [RawAnnotation "JsonClassDiscriminator(\"tag\")"]
             , contentsFieldName = "contents"
-            , tagFieldName = "contents"
+            , tagFieldName = "tag"
             }
       }
   )
